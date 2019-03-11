1d5c1a2cc77e22a7342565ee599007a69edf0766
Boruch Z <philomath613@gmail.com>
2019-03-11 13:27:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,18 +1,20 @@
 class TicTacToe
 
+  WIN_COMBINATIONS = [
+    [0, 1, 2],  # Top row
+    [3, 4, 5],  # Middle row
+    [6, 7, 8],  # Bottom
+    [0, 3, 6],  # left column
+    [1, 4, 7],  # Middle column
+    [2, 5, 8],   # Right column
+    [0, 4, 8],  #backslash
+    [2, 4, 6]   #forward_slash
+  ]
+  
   def initialize
     @board = Array.new(9, " ")
 
-    WIN_COMBINATIONS = [
-      [0, 1, 2],  # Top row
-      [3, 4, 5],  # Middle row
-      [6, 7, 8],  # Bottom
-      [0, 3, 6],  # left column
-      [1, 4, 7],  # Middle column
-      [2, 5, 8],   # Right column
-      [0, 4, 8],  #backslash
-      [2, 4, 6]   #forward_slash
-    ]
+  
   end
 
   def display_board
